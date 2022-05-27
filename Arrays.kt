2d4605ee2989fb7061793e75685fc046d3a394eb
AntonPolotovsky <rmt2018@yande.ru>
2022-05-27 08:08:48 +0400

    In this variant of my program, I have refactored code.

--- Arrays.kt
+++ Arrays.kt
@@ -9,396 +9,240 @@ import kotlin.math.min
 fun toCharacterArray(str: String): Array<Char> {
     return str.toCharArray().toTypedArray()
 }
+
 fun main() {
 }
-    // Задача 1
-fun lessFive () { //1
-        lessFiveVarArg(*intArrayOf(1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89))
+
+// Задача 1
+fun lessFive() { //1
+    try {
+        lessFiveVarArg(readLine()!!.toString().split(",").map { it.toInt() })
+        {
+            it <= 5
+        }
+    } catch (e: java.lang.NumberFormatException) {
+        println("Введите верно")
     }
-fun lessFiveVarArg (vararg numbers :Int) {
-//val numbers = arrayOf(1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89)
+}
+
+fun lessFiveVarArg(numbers: List<Int>, condition: (Int) -> Boolean) {
     for (t in numbers) {
-        if (t <= 5) {
-            println(t)
+        if (condition(t)) {
+            print("$t ")
         }
     }
-    }
+    println()
+}
 
-    //Задача 2
-fun commonElements () { //2
-        val numbers = arrayOf(1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89)
-        val numbers2 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
-        for (t in numbers) {
-            for (n in numbers2) {
-                if (t == n) {
-                    println(t)
-                }
+//Задача 2
+fun commonElements() { //2
+    try {
+        elements(
+            readLine()!!.toString().split(",").map { it.toInt() },
+            readLine()!!.toString().split(",").map { it.toInt() })
+        { t, n ->
+            t == n
+        }
+    } catch (e: NumberFormatException) {
+        println("Введите верно")
+    }
+}
+
+inline fun elements(numbers: List<Int>, numbers2: List<Int>, condition: (Int, Int) -> Boolean) { //2
+     for (t in numbers) {
+        for (n in numbers2) {
+            if (condition(t, n)) {
+                print("$t ")
             }
-
         }
+
     }
+    println()
+}
 
-    //Задача 3
-fun sort () { //3
-        sortNum (1, 10, 15, 2, 3, 4, 7, 5, 8, 13, 21, 152, 34, 55, 89)
+fun sort() { //3
+    try {
+        sortNum(readLine()!!.toString().split(",").map { it.toInt() })
+    } catch (e: java.lang.NumberFormatException) {
+        println("Введите верно")
     }
-fun sortNum (vararg numbers :Int) {
-    // val numbers = arrayOf(1, 10, 15, 2, 3, 4, 7, 5, 8, 13, 21, 152, 34, 55, 89)
-
-    numbers.sort()
-    println("Sorted array: ${numbers.contentToString()}")
-    numbers.sortDescending()
-    println("Sorted array: ${numbers.contentToString()}")
+}
+fun sortNum(numbers: List<Int>) {
+    println(numbers)
+    val t = numbers.sorted()
+    println(t)
+    val r = t.sortedDescending()
+    println(r)
+}
+fun merging() { //4
+    try {
+        merge(
+            readLine()!!.toString().split(",").map { it.toInt() },
+            readLine()!!.toString().split(",").map { it.toInt() })
+    } catch (e: java.lang.NumberFormatException) {
+        println("Введите верно")
     }
+}
 
-    //Задача 4
-fun merging () { //4
-        val numbers = arrayOf(1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89)
-        val numbers2 = arrayOf(-1, -3, -9)
-        var numbers3 = numbers + numbers2
-        println(numbers3.asList())
-        numbers3.sort()
-        println("Sorted array: ${numbers3.contentToString()}")
+fun merge(numbers: List<Int>, numbers2: List<Int>) {
+    val numbers3 = numbers + numbers2
+    println(numbers3)
+    val sortedArray = numbers3.sorted()
+    println(sortedArray)
+}
+fun maxMeaning() { //5
+    meanings(readLine()!!.toString().split(",").map { it.toInt() })
+}
+fun meanings(numbers: List<Int>) {
+    val sortedArray = numbers.sorted()
+    val reversArray = sortedArray.sortedDescending()
+    println("Sorted array: ${reversArray.take(3)}")
+}
+fun polydrom() { //6
+    polydromAct(readLine()!!.toString())
+}
+fun polydromAct(str: String) {
+    val chars: Array<Char> = toCharacterArray(str)
+    val reversedChars = chars.reversedArray()
+    if (chars.contentEquals(reversedChars)) {
+        println("Слово является полиндромом.")
+    } else {
+        println("Слово не является полиндромом.")
     }
-
-    //Задача 5
-    fun maxMeaning () { //5
-        val numbers = arrayOf(500, 5874, 560, 400, 5874, 20)
-        numbers.sortDescending()
-
-
-        println("Sorted array: ${numbers.take(3)}")
-    }
-    //Задача 6???????????????????????????????????
-
-//    val a: Int = 651
-//    val s = Integer.toHexString(a)
-//    var d = a.toString()
-//    println(s)
-//    println(d)
-
-    //Задача 7 треугольник паскаля ????????????
-
-//
-//    fun computeTriangle(n: Int): List<List<Int>> {
-//        require(n >= 0) { "Rows can't be negative!" }
-//        return when (n) {
-//            0 -> listOf<List<Int>>()
-//            else -> (2..n).fold(listOf(listOf(1))) { acc, _ ->
-//                acc.plusElement(listOf(1) + acc.last().zipWithNext(Int::plus) + listOf(1))
-//            }
-//        }
-//    }
-
-    //Задача 8
-
-  fun polydrom () { //6
-      println("Введите слово")
-
-      val str = readLine()!!.toString()
-
-      val chars: Array<Char> = toCharacterArray(str)
-      println(chars.contentToString())
-      val reversdeChars = chars.reversedArray()
-      println(reversdeChars.contentToString())
-
-      println(chars.contentEquals(reversdeChars))
-
-  }
-    //Задача 9
-
-fun timeArray () { //7
-    var time = arrayOf(60, 60, 24, 7)
-    println("Enter seconds")
+}
+fun timeArray(a: Int) { //7
+    val time = arrayOf(60, 60, 24, 7)
     var a = readLine()!!.toInt()
     val sec = a % time[0] // вычислили секунды для выводы
     a -= sec
-    a = a / time[0]          //общее количество минут
+    a /= time[0]          //общее количество минут
     val min = a % time[1]
-    a = a - min
-    a = a / time[1]          //общее количество часов
+    a -= min
+    a /= time[1]          //общее количество часов
     val chas = a % time[2]
-    a = a - chas
-    a = a / time[2]          //общее количество дней
+    a -= chas
+    a /= time[2]          //общее количество дней
     val dni = a % time[3]
     println("Дни: $dni, Часы: $chas, Минуты: $min, Секунды: $sec.")
 }
 
-    //Задача 10
-
-fun numArray () { //8
-    println("Введите три цифры через запятую")
-
-    val strArray = readLine()!!.toString().split(",").toTypedArray()
+fun numArray(strArray: Array<String>) { //8
     println(strArray[0])
     println(strArray[1])
     println(strArray[2])
-    println(strArray[0].toInt() * strArray[2].toInt())
 }
 
-
-    //Задача 11
-
-fun firsLastElement () { //9
-    println("Введите цифры через запятую")
-    val strArray = readLine()!!.toString().split(",").toTypedArray()
-    println(strArray[0])
-//   val reversdeChars = strArray.reversedArray()
-//   println(reversdeChars.contentToString()[1])
-
-    println(strArray.last())
+fun firsLastElement(strArray: Array<String>) { //9
+    println("Первое число массива: " + strArray[0])
+    println("Последнее число массива: " + strArray.last())
 }
 
-    //Задача 12?????????????????????????
+fun plusCount(a: Int) { //10
+    val b = a
+    val c = (a + a)
+    val d = (a + a + a)
+    val sum = b + c + d
+    println("ответ по формуле: a+aa+aaa равен: $sum")
+}
 
-    /*println("Введите имя файла")
-    var strArray = readLine()!!.toString()
-    var parts = strArray.split('.')
+fun evenNumbers(array: List<Int>) { //11
+    for (x in array) {
+        if (x == 237) {
+            break
+        } else if (x % 2 == 0) {
 
-    println(parts[1])*/
-
-//    if (parts.equals(2)) {
-//            println(parts[1])
-//        }
-//    else {
-//        println("Неверное имя файла")
-//    }
-//
-//    if ()
-//    var strArray = readLine()!!.toString().split(".").toTypedArray()
-
-//    if (strArray != 2) {
-
-
-    //Задача 13
-    fun plusCount () { //10
-        println("Введите значение a ")
-        var a = readLine()!!.toString()
-        var b = a.toInt()
-        var c = (a + a).toInt()
-        var d = (a + a + a).toInt()
-        var sum = b + c + d
-        println("ответ по формуле: a+aa+aaa равен: $sum")
-    }
-
-    //Задача 14
-fun evenNumbers () { //11
-        val array = arrayOf(254, 24, 97, 817, 432, 748, 32, 656, 321, 3463, 856, 237, 34, 324, 63)
-        for (x in array) {
-            if (x == 237) {
-                break
-            } else if (x % 2 == 0) {
-
-                println(x)
-            }
+            println(x)
         }
     }
+}
 
-    //Задача 15
-    fun nonElements () { //12
-    val numbers = arrayOf(1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89)
-    val numbers2 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+fun nonElements(numbers: List<Int>, numbers2: List<Int>, condition: (Int, Int) -> Boolean) { //12
     for (number in numbers) {
-        var count = 0
-        for (number2 in numbers2) {
-            if (number == number2) {
+         var count = 0
+         for (number2 in numbers2) {
+            if (condition(number,number2)) {
                 break
-            } else {
-                count++
-                if (count == numbers2.size) {
-                    println(number)
+             } else {
+                 count++
+                 if (count == numbers2.size) {
+                     println(number)
                 }
             }
         }
     }
-    }
+}
 
-    //Задача 16
-    fun listOfElements () { //13
-        varArgFunction(1, 2, 3, 4, 5, 6, 7)
-    }
-fun varArgFunction (vararg numbers: Int) {
-//val numbers = arrayOf(1, 2, 3, 4, 5)
-
+fun listOfElements(vararg numbers: List<Int>) { //13
     for (number in numbers)
         print("$number ")
-
 }
 
-    //Задача 17
-  fun numbersSum () { //14
-        println("Введите число ")
-        val word = readLine()!!.toString()
-        var result: Long = 0
-        for (number in word) {
-            val digit = number - '0'
-            result += digit
-        }
-        println(result)
+fun numbersSum(word: String) { //14
+    var result: Long = 0
+    for (number in word) {
+        val digit = number - '0'
+        result += digit
     }
+    println(result)
+}
 
+inline fun countSymbol(numbers: List<Int>, num: Int, condition : (Int, Int) -> Boolean) { //15
+    var count = 0
+    for (x in numbers) {
+        if (condition(x,num)) {
+            count += 1
+        }
+    }
+    println("Количество повторений числа в массиве: $count.")
+}
 
-//    val str = readLine()!!.toString()
-//    var sum = 0
-//    for(c in str) {
-//        val digit = c - '0'
-//
-//            sum += digit
-//    }
-//    println (sum)
+fun changePosition(x: Int, y: Int) { //16
+    val c = y
+    val y = x
+    val x = c
+    println("После смены значений переменных, x = $x, y = $y")
+}
 
-    //Задача 18
-    fun countSymbol () { //15
-        val num = 1
-        val numbers = arrayOf(1, 1, 2, 3, 4, 5, 1, 8, 13, 21, 34, 55, 89)
+inline fun quarter(array: List<Int>, condition: (Int) -> Boolean)  //17
+{
+    for (i in array) {
+        if (condition(i))
+            println(i)
+    }
+}
+
+inline fun unicNumbers(array: List<Int>, condition: (Int, Int) -> Boolean) {
+    print("Уникальные числа из массива: ")
+    for (number in array) {
         var count = 0
-        for (x in numbers) {
-            if (x == num) {
-                count += 1
-
-            }
-
-        }
-
-        println(count)
-    }
-
-    //Задача 19
-    fun changePosition () { //16
-        println("Введите значение Х: ")
-        var x = readLine()!!.toString()
-        println("Введите значение Y: ")
-        var y = readLine()!!.toString()
-        var c = y
-        y = x
-        x = c
-        println("После смены значений переменных, x = $x, y = $y")
-    }
-
-    //Задача 20
-    fun quater () { //17
-        var array = arrayOf(15, 20, 30, 21, 45, 84, 60, 90, 100)
-        for (i in array) {
-            if (i % 15 == 0)
-                println(i)
-        }
-    }
-
-    //Задача 21
-    fun unicNumbers () { //18
-        val array = arrayOf(15, 20, 15, 30, 21, 45, 15, 84, 60, 90, 100)
-        for (number in array) {
-            var count = 0
-            for (otherNumber in array) {
-                if (number != otherNumber) {
-                    count++
-                    if (count == array.lastIndex) {
-                        println(number)
-                    }
+        for (otherNumber in array) {
+            if (condition(number, otherNumber)) {
+                count++
+                if (count == array.lastIndex) {
+                    print("$number ")
                 }
             }
         }
     }
+}
 
-    //Задача 22
-    fun longAndOften () { //19
-        textNew("good", "morning", "good", "my", "name", "is", "Anton", "Anton", "Anton")
-    }
-fun textNew(vararg text: String) {
-
+inline fun longAndOften(text: Array<String>, condition: (String, String) -> Boolean) { //19
     var maxLetterWord = ""
-    var wordWord = ""
     val words = text
     for (word in text) {
         var count = 1
-        var count2 = 1
-        var maxCount = 0
-        var maxWord = ""
-        var wWord = ""
-        //println(word.count()) //количество букв в слове
         for (otherWord in words) {
-                if (word.count() > otherWord.count()) {
-                    count++
-                    maxLetterWord = word
-                }
-                if (count == words.size) {
-                    println("The longest word -> $maxLetterWord")
-                }
+            if (condition(word, otherWord)) {
+                count++
+                maxLetterWord = word
+            }
+            if (count == words.size) {
+                println("The longest word -> $maxLetterWord")
             }
         }
-
-
-        val countByElement = words.groupingBy { it }.eachCount()
-        val maximumElement = countByElement.maxByOrNull { it.value }?.key
-        println("The most common word -> $maximumElement")
-
     }
-
-
-
-
-
-//            if (word.equals(otherWord)) {
-//                count2++
-//                }
-//            else {
-//                if (count2 > maxCount) {
-//                   maxCount = count
-//                   maxWord = word
-//                }
-//            wWord = word
-//                count2 = 1
-//            }
-//
-//        }
-//
-//        if (count > maxCount) {
-//            maxCount = count
-//            maxWord = word
-//
-//        }
-//        println(maxWord)
-//    }
-
-
-
-
-
-//    var chastoe = 0
-//    var kolvo = 0
-//    for (i in text) {
-//        if (text.count(i) > kolvo){
-//            kolvo = text.count(i)
-//            chastoe = i
-//        }
-//    }
-
-//    for (word in words) {
-//        var count2 = 0
-//        for (otherWord in words) {
-//            if (word == otherWord){
-//                count2++
-//                wordWord = word
-//            }
-//            if (count2 == words.size) {
-//                println(wordWord)
-//            }
-//
-//        }
-//
-//    }
-//    println(wordWord)
-
-
-
-
-
-
-
-
-//    }
-
-
-
-
+    val countByElement = words.groupingBy { it }.eachCount()
+    val maximumElement = countByElement.maxByOrNull { it.value }?.key
+    println("The most common word -> $maximumElement")
+}
 
--- logicConstrtions.kt
+++ logicConstrtions.kt
@@ -1,83 +1,41 @@
 package MyProjectForGithub
 
-fun usualCompare (a: Int, b: Int) {
-//        println("Введите первое число: ")
-//        val a = readLine()!!.toInt()
-//        println("Введите второе число: ")
-//        val b = readLine()!!.toInt()
-      if (a == b) {
-         println("Числа равны")
-     } else if (a < b) {
-         println("Первое число меньше второго")
-     } else {
-         println("Первое число больше второго")
-     }
-
-}
-
-
-// Задача 2
-fun specifiedCompare (a: Int) {
-//        println("Введите число: ")
-//        val a = readLine()!!.toInt()
-    if (a > 2) {
-        if (a < 9) {
-            println("Число больше 5 и меньше 10")
-
-        } else {
-            //!!!!!почему если else стоит в первом if-до него не доходит выполнение условий
-            println("Неизвестное число")
-        }
+fun usualCompare(a: Int, b: Int, condition: (Int, Int) -> Boolean) {
+    if (condition(a, b)) {
+        println("Числа равны")
+    } else if (a < b) {
+        println("Первое число меньше второго")
+    } else {
+        println("Первое число больше второго")
     }
 }
 
-/*if ((a>2) and (a<9)){                     \\!!!!!почему, если не and, а or - условие до else не доходит
-    println("Число больше 5 и меньше 10")
+fun specifiedCompare(a: Int, condition: (Int) -> Boolean) {
+    if (condition(a)) {
+        println("Число больше 5 и меньше 10")
+    } else {
+        println("Неизвестное число")
+    }
 }
 
-    else {
-        println("Неизвестное число")
-    }*/
-
-
-// Задача 3
-fun variationOfDeposit (a: Int) {
-//        println("Введите сумму вклада: ")
-//        val a = readLine()!!.toInt()
+fun variationOfDeposit(a: Int) {
     when {
         (a <= 100) -> println("Сумма вклада с процентами: ${a * 1.05}")
-        ((a > 100) and (a <= 200)) -> println("Сумма вклада с процентами: ${a * 1.07}")
+        ((a > 101) and (a <= 200)) -> println("Сумма вклада с процентами: ${a * 1.07}")
         (a > 200) -> println("Сумма вклада с процентами: ${a * 1.1}")
     }
 }
 
-
-
-// Задача 4
-fun shortMathOperations (a: Int) {
-// println("Введите номер операции:\n"+
-//            "1. Сложение\n"+
-//            "2. Вычитание\n"+
-//            "3. Умножение")
-//    val a = readLine()!!.toInt()
+fun shortMathOperations(a: Int) {
     when {
-            (a == 1) -> println("Вы выбрали сложение")
-            (a == 2) -> println("Вы выбрали вычитание")
-            (a == 3) -> println("Вы выбрали умножение")
+        (a == 1) -> println("Вы выбрали сложение")
+        (a == 2) -> println("Вы выбрали вычитание")
+        (a == 3) -> println("Вы выбрали умножение")
+        else -> println("Операция не определена")
+    }
+}
 
-            else -> println("Операция не определена")
-        }
-
-        }
-
-
-
-//Задача 5
-fun smalMathProgramm (b: Int, c: Int) {
-//        println("Введите первое число")
-//        val b = readLine()!!.toInt()
-//        println("Введите второе число")
-//        val c = readLine()!!.toInt()
+fun smalMathProgramm(b: Int, c: Int) {
     println(
         "Введите номер операции:\n" +
                 "1. Сложение\n" +
@@ -85,7 +43,6 @@ fun smalMathProgramm (b: Int, c: Int) {
                 "3. Умножение"
     )
     val a = readLine()!!.toInt()
-
     when {
         (a == 1) -> println("Результат операции: ${b + c}")
         (a == 2) -> println("Результат операции: ${b - c}")
@@ -94,13 +51,7 @@ fun smalMathProgramm (b: Int, c: Int) {
     }
 }
 
-
-// задача 6
-fun easyCalculator (b: Double, c: Double) {
-//        println("Введите первое число")
-//        val b = readLine()!!.toDouble()
-//        println("Введите второе число")
-//        val c = readLine()!!.toDouble()
+fun easyCalculator(b: Double, c: Double) {
     println(
         "Введите номер операции:\n" +
                 "1. Сложение\n" +
@@ -110,7 +61,6 @@ fun easyCalculator (b: Double, c: Double) {
                 "5. Остаток отцелочисленного деления\n"
     )
     val a = readLine()!!.toInt()
-
     when {
         (a == 1) -> println("Результат сложения: ${b + c}")
         (a == 2) -> println("Результат вычитания: ${b - c}")
@@ -120,46 +70,28 @@ fun easyCalculator (b: Double, c: Double) {
         else -> println("Операция не определена")
     }
 }
-fun timeCalculator (seconds: Int) {
 
-//    println("Введите некоторое количество секунд: ")
-//    var a = readLine()!!.toInt()
+fun timeCalculator(seconds: Int) {
     var a = seconds
     val sec = a % 60 // вычислили секунды для выводы
     a -= sec
-    //println("seconds $sec")
-    //println(a)
-    a = a / 60          //общее количество минут
+    a /= 60          //общее количество минут
     val min = a % 60
-    a = a - min
-    //println("minutes $min")
-    //println(a)
-    a = a / 60          //общее количество часов
+    a -= min
+    a /= 60          //общее количество часов
     val chas = a % 24
-    a = a - chas
-    //println("chasi $chas")
-    //println(a)
-    a = a / 24          //общее количество дней
+    a -= chas
+    a /= 24          //общее количество дней
     val dni = a % 7
-    a = a - dni
-    //println("dni $dni")
-    //println(a)
-    a = a / 7          //общее количество недель
+    a -= dni
+    a /= 7          //общее количество недель
     val ned = a % 4
-    a = a - ned
-    //println("nedeli $ned")
-    //println(a)
-    a = a / 4          //общее количество месяцев
+    a -= ned
+    a /= 4          //общее количество месяцев
     val mes = a % 12
-    a = a - mes
-    //println("mesyatsev $mes")
-    //println(a)
-    a = a / 12          //общее количество лет
+    a -= mes
+    a /= 12          //общее количество лет
     val god = a
-    //a = a - mes
-    //println("godi $god") 1247896548
-    //println(a)
-    //println("$god г.  $mes м. $ned  н. $dni д.  $chas ч.  $min м. $sec с.")
     when (god) {
         1, 21, 31, 41, 51, 61, 71, 91 -> print("$god Год, ")
         2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54, 62, 63, 64, 72, 73, 74, 82, 83, 84, 92, 93, 94 -> print(
@@ -167,7 +99,6 @@ fun timeCalculator (seconds: Int) {
         )
         else -> {
             print("$god Лет, ")
-
         }
     }
     when (mes) {
@@ -180,10 +111,9 @@ fun timeCalculator (seconds: Int) {
     when (ned) {
         1 -> print("$ned Неделя, ")
         2, 3, 4 -> print("$ned Недели, ")
-
     }
     when (dni) {
-        1, -> print("$dni День, ")
+        1 -> print("$dni День, ")
         2, 3, 4 -> print("$dni Дня, ")
         else -> {
             print("$dni Дней, ")
@@ -195,7 +125,6 @@ fun timeCalculator (seconds: Int) {
         else -> {
             print("$chas Часов, ")
         }
-
     }
     when (min) {
         1, 31, 41, 51 -> print("$min Минута, ")
@@ -209,83 +138,68 @@ fun timeCalculator (seconds: Int) {
         2, 3, 4, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> println("$sec Секунды.")
         else -> {
             println("$sec Секунд.")
-
         }
     }
 }
-    fun currencyConverter (rub: Int) {
-//    println("Введите колчество рублей")
-//    val rub = readLine()!!.toInt()
 
-        println(
-            "Введите Валюту:\n" +
-                    "1. Доллары\n" +
-                    "2. Евро\n" +
-                    "3. Фунт"
-        )
-        val nazv = readLine()!!.toInt()
-        //if (nazv == 1 or 2 or 3) {
-        if (nazv >= 4 || nazv < 1) {
-            println("Вы не верно выбрали валюту")
-        } else if (nazv == 1) {
-            println("Введите курс")
-            val kurs = readLine()!!.toInt()
-            val denigi = rub / kurs
-            val rez1 = denigi % 100
-            val rez2 = denigi % 10
-
-            if (rez1 in 11..20) {
-                print("$denigi Долларов")
-            } else if (rez2 == 1) {
-                print("$denigi Доллар.")
-            } else if (rez2 in 2..4) {
-                print("$denigi Доллара.")
-            } else {
-                print("$denigi Долларов.")
-            }
-
+fun currencyConverter(rub: Int, condition: (Int) -> Boolean) {
+    println(
+        "Введите Валюту:\n" +
+                "1. Доллары\n" +
+                "2. Евро\n" +
+                "3. Фунт"
+    )
+    val nazv = readLine()!!.toInt()
+    if (condition(nazv)) {
+        println("Вы не верно выбрали валюту")
+    } else if (nazv == 1) {
+        println("Введите курс")
+        val kurs = readLine()!!.toInt()
+        val denigi = rub / kurs
+        val rez1 = denigi % 100
+        val rez2 = denigi % 10
+        if (rez1 in 11..20) {
+            print("$denigi Долларов")
+        } else if (rez2 == 1) {
+            print("$denigi Доллар.")
+        } else if (rez2 in 2..4) {
+            print("$denigi Доллара.")
+        } else {
+            print("$denigi Долларов.")
         }
 
-
-        //println("Сумма в долларах: ${rub/kurs}")
-
-        else if (nazv == 2) {
-            println("Введите курс")
-            val kurs = readLine()!!.toInt()
-            val denigi = rub / kurs
-            val rez1 = denigi % 100
-            val rez2 = denigi % 10
-
-            if (rez1 in 11..20) {
-                print("$denigi Евро")
-            } else if (rez2 == 1) {
-                print("$denigi Евро.")
-            } else if (rez2 in 2..4) {
-                print("$denigi Евро.")
-            } else {
-                print("$denigi Евро.")
-            }
-
-
-        } else if (nazv == 3) {
-            println("Введите курс")
-            val kurs = readLine()!!.toInt()
-            val denigi = rub / kurs
-            val rez1 = denigi % 100
-            val rez2 = denigi % 10
-
-            if (rez1 in 11..20) {
-                print("$denigi Фунтов")
-            } else if (rez2 == 1) {
-                print("$denigi Фунт.")
-            } else if (rez2 in 2..4) {
-                print("$denigi Фунта.")
-            } else {
-                print("$denigi Фунтов.")
-            }
-
+    } else if (nazv == 2) {
+        println("Введите курс")
+        val kurs = readLine()!!.toInt()
+        val denigi = rub / kurs
+        val rez1 = denigi % 100
+        val rez2 = denigi % 10
+        if (rez1 in 11..20) {
+            print("$denigi Евро")
+        } else if (rez2 == 1) {
+            print("$denigi Евро.")
+        } else if (rez2 in 2..4) {
+            print("$denigi Евро.")
+        } else {
+            print("$denigi Евро.")
         }
-        println()
+    } else if (nazv == 3) {
+        println("Введите курс")
+        val kurs = readLine()!!.toInt()
+        val denigi = rub / kurs
+        val rez1 = denigi % 100
+        val rez2 = denigi % 10
 
+        if (rez1 in 11..20) {
+            print("$denigi Фунтов")
+        } else if (rez2 == 1) {
+            print("$denigi Фунт.")
+        } else if (rez2 in 2..4) {
+            print("$denigi Фунта.")
+        } else {
+            print("$denigi Фунтов.")
+        }
     }
+    println()
+}
 
